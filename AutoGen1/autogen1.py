3426cd0758b8c7efc8ec3a0e87615e05e3549cf9
Daniel Bicknell <danielbicknell63@gmail.com)>
2025-02-15 11:13:53 -0600

    Updating script

--- AutoGen1/autogen1.py
+++ AutoGen1/autogen1.py
@@ -2,11 +2,14 @@ import os
 import autogen
 from autogen import ConversableAgent
 
+#import OpenAI API Key
 config_list = autogen .config_list_from_json(env_or_file="connections\OAI_CONFIG_LIST")
 
+
+# creaete conversable agent
 agent = ConversableAgent(
     "chatbot",
-    llm_config={"config_list": [{}]},
+    llm_config={"config_list": config_list},
     code_execution_config=False,  # Turn off code execution, by default it is off.
     function_map=None,  # No registered functions, by default it is None.
     human_input_mode="NEVER",  # Never ask for human input.
