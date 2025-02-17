28ccf8b3c925f1c7b27b8107367a734c173f9e75
Daniel Bicknell <danielbicknell63@gmail.com)>
2025-02-17 12:39:45 -0600

    Adding OpenAI

--- OpenAI/OpenAI1.py
+++ OpenAI/OpenAI1.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 
 client = OpenAI(
-  api_key="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"
+  api_key=API_KEY
 )
 
 completion = client.chat.completions.create(
